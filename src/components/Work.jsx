7b48305529beff4110650a977e29dec7e5fe9a4e
qasimroy <qasimroy5234@gmail.com>
2023-09-11 13:14:01 +0500

    transition added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,7 +21,7 @@ const Work = () => {
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
                          mx-auto content-div"
                     >
-                        <div className="opacity-0 group-hover:opacity-100">
+                        <div className="opacity-0 group-hover:opacity-100 transition duration-300">
                             <span className="text-2xl font-bold text-white tracking-wider">
                                 Movie DB Website
                             </span>
@@ -50,7 +50,7 @@ const Work = () => {
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
                          mx-auto content-div"
                     >
-                        <div className="opacity-0 group-hover:opacity-100">
+                        <div className="opacity-0 group-hover:opacity-100  transition duration-300">
                             <span className="text-2xl font-bold text-white tracking-wider">
                                 Captain Taxi Website
                             </span>
